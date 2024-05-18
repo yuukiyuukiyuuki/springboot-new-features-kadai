68639d91cda92ceb323a4bffea30eaae94fdd21a
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2024-05-18 09:39:53 +0900

    追加

---
+++ kadai_003/java/com/example/samuraitravel/form/FavoriteRegisterForm.java
@@ -0,0 +1,19 @@
+package com.example.samuraitravel.form;
+
+/*@Data
+@NoArgsConstructor
+public class FavoriteRegisterForm {
+	
+	@NotNull
+	private Integer houseId;
+	
+	@NotNull
+	private Integer userId;
+	
+
+	@Transactional
+	public void deleteByHouseIdAndUserId(Integer houseId, Integer userId) {
+		this.houseId = houseId;
+		this.userId = userId;
+	}*/
+//}
